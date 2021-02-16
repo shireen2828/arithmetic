d73eca84804bee4491a378bd8597aff205b2d27e
unknown <shireenkk28@gmail.com>
2021-02-16 21:34:06 +0530

    added arithmetic.sh

--- arithmetic.sh
+++ arithmetic.sh
@@ -1,6 +1,8 @@
+
 #! /bin/bash -x
 
 declare -A sum
+declare -a array
 
 read -p "enter 1st number" a
 read -p "enter 2nd number" b
@@ -10,6 +12,13 @@ sum[mul]=$(( (a*b)+c ))
 sum[div]=$(( (c+a)/b ))
 sum[mod]=$(( (a%b)+c ))
 echo ${sum[@]}
+i=0
+for key in ${!sum[@]}
+do
+	array[$i]=${sum[$key]}
+	i=$(( i+1 ))
+done
+echo ${array[@]}
 
 
 
